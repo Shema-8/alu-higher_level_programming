79493868160ac28e79548a326bf27fb718f805f8
Shema-8 <e.shema2@alustudent.com>
2025-02-09 02:22:10 -0800

    Hello print

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,2 +1,2 @@
-#!/bin/bash
-PYCODE='print("Programming is like building a multilingual puzzle")'
+#!/usr/bin/python3
+PYCODE='print("Programming is like building a multilingual puzzle")' bash -c 'python3 -c "$PYCODE"'
